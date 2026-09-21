726e8291817811a020eec47098cee779c9a49276
MoniqueRISE <madline.av@jeffersonrise.org>
2026-09-21 14:08:09 +0000

    error with head tag

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -4,6 +4,13 @@ $(function () {
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
 
+// createPlatform
+createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
+createPlatform(100, 20, 10, 10, "lime"); // bright green for a finished platform
+
+
+
   function setup() {
     if (firstTimeSetup) {
       halleImage = document.getElementById("player");
@@ -31,27 +38,21 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-    createPlatform(500, 0, 20, 290);
-    createPlatform(1350, 400, 50, 50, "red");
-    createPlatform(100, 20, 10, 10, "lime"); // bright green for a finished platform
-
+   
 
 
 
 
 
     // TODO 3 - Create Collectables
-    createCollectable("steve", 1350, 50);
-    createCollectable("diamond", 200, 170, 0.5, 0.7);
+   
 
 
 
 
     
     // Cannons
-    createCannon("top", 200, 100);
-    createCannon("right", 300, 2000);
-
+   
 
     
     
